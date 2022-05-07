8ba028d4919b980a408bd611d310ce8c171591d3
Lawal Abdulmujeeb <lawalmujeeb78@gmail.com>
2022-05-07 15:19:21 +0100

    test bootstrap

---
+++ src/App.js
@@ -0,0 +1,8 @@
+export default function App() {
+    return (
+        <div>
+            <h1>This is the App Component</h1>
+            <button type="button" className="btn btn-primary">Primary</button>
+        </div>
+    )
+}
