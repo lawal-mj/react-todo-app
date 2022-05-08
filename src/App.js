6485bff21df90151fe85d77897869b991d3c94a7
Lawal Abdulmujeeb <lawalmujeeb78@gmail.com>
2022-05-08 16:34:45 +0100

    add input state and update function

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import Header from "./components/Header";
 import Input from "./components/Input";
 import Task from "./components/Task";
+import { useState } from "react";
 
 
 export default function App(){
+
+    const [ inputValue, setInputValue ] = useState("")
+    function updateInput(event){
+        setInputValue(event.target.value)
+    }
+
+    console.log(inputValue)
+
     return (
         <div className="container-fluid todo-container">
             <Header />
-            <Input />
+            <Input 
+                update= {updateInput}
+            />
             <Task />
         </div>
     )
--- src/components/Input.js
+++ src/components/Input.js
@@ -1,14 +1,13 @@
-export default function Input() {
-    return (
-            <div className="task-head">
-                <input placeholder="Enter new task" className="input" type="text" />
-                <button className="task-button">Add</button>
-            </div>
-
-    )
-
+export default function Input(props) {
+  return (
+    <div className="task-head">
+      <input 
+      placeholder="Enter new task"
+       className="input"
+        type="text" 
+        onChange={props.update}
+        />
+      <button className="task-button">Add</button>
+    </div>
+  );
 }
-
-
-
-
