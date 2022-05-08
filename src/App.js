0399ab131a6cf932da40935aaa9f6cf364392225
Lawal Abdulmujeeb <lawalmujeeb78@gmail.com>
2022-05-08 16:36:56 +0100

    remove bootstrap cdn from html file

--- src/App.js
+++ src/App.js
@@ -3,24 +3,19 @@ import Input from "./components/Input";
 import Task from "./components/Task";
 import { useState } from "react";
 
+export default function App() {
+  const [inputValue, setInputValue] = useState("");
+  function updateInput(event) {
+    setInputValue(event.target.value);
+  }
 
-export default function App(){
+  console.log(inputValue);
 
-    const [ inputValue, setInputValue ] = useState("")
-    function updateInput(event){
-        setInputValue(event.target.value)
-    }
-
-    console.log(inputValue)
-
-    return (
-        <div className="container-fluid todo-container">
-            <Header />
-            <Input 
-                update= {updateInput}
-            />
-            <Task />
-        </div>
-    )
-
+  return (
+    <div className="container-fluid todo-container">
+      <Header />
+      <Input update={updateInput} />
+      <Task />
+    </div>
+  );
 }
--- src/components/Input.js
+++ src/components/Input.js
@@ -1,12 +1,12 @@
 export default function Input(props) {
   return (
     <div className="task-head">
-      <input 
-      placeholder="Enter new task"
-       className="input"
-        type="text" 
+      <input
+        placeholder="Enter new task"
+        className="input"
+        type="text"
         onChange={props.update}
-        />
+      />
       <button className="task-button">Add</button>
     </div>
   );
