2c8f230dea7b1897decc0c02743f51329b3a8e57
Lawal Abdulmujeeb <lawalmujeeb78@gmail.com>
2022-05-09 11:47:59 +0100

    remove extra tasks

--- src/components/Task.js
+++ src/components/Task.js
@@ -11,59 +11,7 @@ export default function Task() {
             </div>
           </li>
         </div>
-
-        <div className="task--item">
-          <li className="list-group-item d-flex justify-content-between align-items-start">
-            <div className="m-1 me-auto">
-              <div className="fw-bold">
-                Subheading<button className="delete--task">Delete</button>
-              </div>
-            </div>
-          </li>
-        </div>
-
-        <div className="task--item">
-          <li className="list-group-item d-flex justify-content-between align-items-start">
-            <div className="m-1 me-auto">
-              <div className="fw-bold">
-                Subheading<button className="delete--task">Delete</button>
-              </div>
-            </div>
-          </li>
-        </div>
       </div>
     </div>
   );
-}
-
-{
-  /* <div className="tasks">
-    <div className="task--item">
-        <li className="list-group-item d-flex justify-content-between align-items-start">
-            <div className="m-1 me-auto">
-                <div className="fw-bold">Subheading<button className="delete--task">Delete</button></div>
-
-            </div>
-        </li>
-    </div>
-
-    <div className="task--item">
-        <li className="list-group-item d-flex justify-content-between align-items-start">
-            <div className="m-1 me-auto">
-                <div className="fw-bold">Subheading<button className="delete--task">Delete</button></div>
-
-            </div>
-        </li>
-    </div>
-
-    <div className="task--item">
-        <li className="list-group-item d-flex justify-content-between align-items-start">
-            <div className="m-1 me-auto">
-                <div className="fw-bold">Subheading<button className="delete--task">Delete</button></div>
-
-            </div>
-        </li>
-    </div>
-
-</div> */
 }
