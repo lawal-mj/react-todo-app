9075ada53d1be297543825e2b29bde484c1cee2e
Lawal Abdulmujeeb <lawalmujeeb78@gmail.com>
2022-05-09 11:20:08 +0100

    added addTask fucntion

--- src/App.js
+++ src/App.js
@@ -3,26 +3,27 @@ import Input from "./components/Input";
 import Task from "./components/Task";
 import { useState } from "react";
 
-const todoList = []
+const todoList = [];
 
 export default function App() {
-  const [inputValue, setInputValue] = useState(todoList);
+  const [inputValue, setInputValue] = useState("todoList");
 
   function updateInput(event) {
-    setInputValue(function(oldInput){
-        return oldInput.push(event.target.value)
+    setInputValue((prevValue) => {
+      return event.target.value;
     });
+  }
+
+  function addTask() {
+    todoList.push(inputValue);
+    console.log(todoList)
 
   }
-  // todoList.push(inputValue)
-  console.log(todoList)
-
-  // console.log(inputValue);
 
   return (
     <div className="container-fluid todo-container">
       <Header />
-      <Input update={updateInput} />
+      <Input update={updateInput} addTask={addTask} />
       <Task />
     </div>
   );
