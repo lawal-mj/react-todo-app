40a0ab647bd5fa4f0934b28c21a2f0048aadd0a1
Lawal Abdulmujeeb <lawalmujeeb78@gmail.com>
2022-05-10 16:54:09 +0100

    glory be to god, error fixed

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ export default function App() {
   });
 
   function AddTask() {
-    const formData = inputData;
+    const formData =[...inputData];
 
     formData.push({
       id: inputData.length + 1,
