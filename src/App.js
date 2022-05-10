020dc5d82d0b49067a6b2394fc50990778153bb5
Lawal Abdulmujeeb <lawalmujeeb78@gmail.com>
2022-05-10 16:14:05 +0100

    not able to fix

--- src/App.js
+++ src/App.js
@@ -2,62 +2,42 @@ import Task from "./components/Task";
 import Input from "./components/Input";
 import { useState } from "react";
 
-const taskObjects = [
+const dataList = [
   {
-    id:1,
+    id: 1,
     task: "Example Task",
-  }
-]
+  },
+];
 
 export default function App() {
+  const [inputBoxValue, setinputBoxValue] = useState("");
 
-
-  const [ inputBoxValue, setinputBoxValue ] = useState("")
-  const [ inputData, setInputData ] = useState(taskObjects)
+  const [inputData, setInputData] = useState(dataList);
 
   const taskList = inputData.map((item) => {
-      return <Task  key={item.id} id={item.id} task={item.task}/>
+    return <Task key={item.id} id={item.id} task={item.task} />;
+  });
 
-  })
-
-
-  function AddTask(){
+  function AddTask() {
     const formData = inputData;
-    const data = {
-      id:inputData.length + 1,
-      task:inputBoxValue
-    }
-    formData.push(data)
 
-    setInputData(function(prevData){
-        return formData
-    })
-    // updateInput()
-    console.log(inputData)
-
+    formData.push({
+      id: inputData.length + 1,
+      task: inputBoxValue,
+    });
+    setInputData(formData);
   }
-
-
 
   function updateInput(event){
-    setinputBoxValue(event.target.value)
-
+      setinputBoxValue(event.target.value)
   }
-
-
-
 
 
 
   return (
     <div>
-      <ul>
-        {taskList}
-      </ul>
-      <Input 
-        updateInput = {updateInput}
-        addTask= {AddTask}
-      />
+      <ul>{taskList}</ul>
+      <Input updateInput={updateInput} addTask={AddTask} />
     </div>
   );
 }
