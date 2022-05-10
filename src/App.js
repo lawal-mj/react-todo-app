e8300f1aef899fa76cf7017028b617f5b98596ac
Lawal Abdulmujeeb <lawalmujeeb78@gmail.com>
2022-05-10 10:41:13 +0100

    fixed issues

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Task from "./components/Task";
+import Input from "./components/Input";
+
+export default function App(){
+  return(
+    <div>
+      <ul>
+        <Task />
+      </ul>
+      <Input />
+    </div>
+  )
+}
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,17 +1,6 @@
-export default function Task(props) {
+export default function Task(){
   return (
-    <div>
-      <div className="tasks">
-        <div className="task--item">
-          <li className="list-group-item d-flex justify-content-between align-items-start">
-            <div className="m-1 me-auto">
-              <div className="fw-bold">
-                {props.task}<button className="delete--task">Delete</button>
-              </div>
-            </div>
-          </li>
-        </div>
-      </div>
-    </div>
-  );
+    <li>Some Task</li>
+  )
+  
 }
