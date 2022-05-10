3d729bc3044695e3a8b22036ddb443928f028807
Lawal Abdulmujeeb <lawalmujeeb78@gmail.com>
2022-05-10 11:32:03 +0100

    able to add new componnts but issue with re render

--- src/components/Input.js
+++ src/components/Input.js
@@ -2,7 +2,7 @@ export default function Input(props) {
   return (
     <div>
       <input type="text" onChange={props.updateInput} />
-      <button onChange={props.updateInput} onClick={props.addTask}>Add Task</button>
+      <button onClick={props.addTask}>Add Task</button>
     </div>
   );
 }
