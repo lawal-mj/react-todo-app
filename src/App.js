795a72674e0fee66e2a663741bd3c4b2c6b3494a
Lawal Abdulmujeeb <lawalmujeeb78@gmail.com>
2022-05-10 06:44:42 +0100

    couldnt find issues

--- src/App.js
+++ src/App.js
@@ -3,43 +3,12 @@ import Input from "./components/Input";
 import Task from "./components/Task";
 import { useState } from "react";
 
-const todoList = ["Example Task"];
-
 export default function App() {
-  const [inputValue, setInputValue] = useState("todoList");
-
-  const TaskList = todoList.map((item) => {
-    return <Task task={item} />;
-  });
-
-
-
-  function updateInput(event) {
-    setInputValue((prevValue) => {
-      return event.target.value;
-    });
-  }
-
-
-
-  function addTask() {
-    if (inputValue === ""){
-  
-    }else {
-      todoList.push(inputValue);
-      setInputValue("")
-
-    }
-
-  }
-
   return (
-    <div className="container-fluid todo-container">
+    <div>
       <Header />
-      <Input update={updateInput} addTask={addTask} />
-      {TaskList}
+      <Input />
+      <Task />
     </div>
   );
 }
-
-
