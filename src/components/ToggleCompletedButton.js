9ac48543b674d39457742b12988d0b2fd519c995
Lawal Abdulmujeeb <lawalmujeeb78@gmail.com>
2022-05-15 07:28:18 +0100

    delete old button components

--- src/components/ToggleCompletedButton.js
+++
@@ -1,7 +0,0 @@
-export default function ToggleCompletedButton(props){
-    return(
-        <button onClick={props.toggle} className="button--toggle--completed">
-                Toggle Completed
-        </button>
-    )
-}
--- src/components/ToggleDeletedButton.js
+++
@@ -1,7 +0,0 @@
-export default function ToggleDeletedButton(props){
-    return(
-        <button onClick={props.toggle} className="button--toggle--deleted">
-                Toggle deleted
-        </button>
-    )
-}
